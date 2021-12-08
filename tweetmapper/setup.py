bbb8c353531e17768262f222d93cc429e686c21f
Bryan Wilson <bryanlandia@gmail.com>
2021-12-07 19:23:41 -0800

    remove README in setup.py

--- tweetmapper/setup.py
+++ tweetmapper/setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
 
 
 version = '0.1'
